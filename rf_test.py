bb51dd35550fb556307704ed59db83b75b1aaece
Patro <vpatro@ndc.nasa.gov>
2023-08-14 00:00:27 -0700

    ran first rf on subset of LHS sampled EMIT data - bad results

--- rf_test.py
+++ rf_test.py
@@ -1,6 +1,36 @@
 from sklearn.ensemble import RandomForestRegressor
+import numpy as np
+import pandas as pd
+from sklearn.model_selection import train_test_split
+import argparse
 
+data = np.load('train_set.npz')
+refls = data['output_rfl']
+wv = data['output_wv']
+fids = data['output_idx']
 
+values, counts = np.unique(fids, return_counts=True)
+scenes = np.split(refls, 946)
+wv_by_scene = np.split(wv, 946)
+scenes = np.array(scenes)
+wv_by_scene = np.array(wv_by_scene)
+    
+X_idx, y_idx = np.arange(10), np.arange(10)
+X_train_idx, X_test_idx, y_train_idx, y_test_idx = train_test_split(X_idx, y_idx, test_size = 0.3, random_state = 50)
+
+X_train = scenes[X_train_idx, :, :]
+X_train = X_train.reshape((X_train.shape[0]*X_train.shape[1], X_train.shape[2]))
+X_test = scenes[X_test_idx, :, :]
+X_test = X_test.reshape((X_test.shape[0]*X_test.shape[1], X_test.shape[2]))
+y_train = wv_by_scene[y_train_idx, :]
+y_train = y_train.flatten()
+y_test = wv_by_scene[y_test_idx, :]
+y_test = y_test.flatten()
+    
 rf = RandomForestRegressor(random_state = 0)
 rf.fit(X_train, y_train)
 y_pred = rf.predict(X_test)
+
+outpath = 'rf_outdir/ss_rf.txt'
+np.savetxt(outpath, y_pred)
+    
